b676790f3b3a70bdfe5a3aad6287ff27401b6983
kms1601 <141806725+kms1601@users.noreply.github.com>
2024-05-14 15:08:18 +0900

    Time: 6 ms (39.55%), Space: 44.1 MB (34.58%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
